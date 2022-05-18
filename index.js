91a0c7bdaf6acabcdce18ba0f60e07feb197f2e4
Justin M. Ramon <justinmramon@gmail.com>
2022-05-18 14:55:09 -0500

    commit tree 6162bfb42b6e7dacd9bb8d25ca2d3707afbe2315

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(skate = 'roller-skate') {
+    return `This Saturday, I want to ${skate}!`
+}
+
+function mondayWork(office = 'go to the office') {
+    return `This Monday, I will ${office}.`
+}
+
+function wrapAdjective(daft = "*"){
+    const punk = function (homework = "special"){
+        return `You are ${daft}${homework}${daft}!`
+    }
+    return punk
+}
